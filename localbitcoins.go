d613b6750b70f4816538d3c1b72a1e77ab0a2fb8
Zach Latta <zach@zachlatta.com>
2013-11-18 02:31:12 -0800

    Run 'go fmt'

--- localbitcoins.go
+++ localbitcoins.go
@@ -1,56 +1,56 @@
 package localbitcoins
 
 import (
-  "bytes"
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  "reflect"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
 
-  "github.com/google/go-querystring/query"
+	"github.com/google/go-querystring/query"
 )
 
 const (
-  libraryVersion = "0.0.1"
-  defaultBaseURL = "https://localbitcoins.com/"
-  userAgent = "go-localbitcoins/" + libraryVersion
+	libraryVersion = "0.0.1"
+	defaultBaseURL = "https://localbitcoins.com/"
+	userAgent      = "go-localbitcoins/" + libraryVersion
 )
 
 // A Client manages communication with the LocalBitcoins API.
 type Client struct {
-  client *http.Client
+	client *http.Client
 
-  // Base URL used for API requests. Must end with a trailing slash.
-  BaseURL *url.URL
+	// Base URL used for API requests. Must end with a trailing slash.
+	BaseURL *url.URL
 
-  // User agent sent when communicating with the LocalBitcoins API.
-  UserAgent string
+	// User agent sent when communicating with the LocalBitcoins API.
+	UserAgent string
 
-  // Services for talking to different parts of the LocalBitcoins API.
+	// Services for talking to different parts of the LocalBitcoins API.
 }
 
 // Adds the parameters in opt as URL query parameters to s. opt must be a
 // struct whose fields may contain "url" tags.
 func addOptions(s string, opt interface{}) (string, error) {
-  v := reflect.ValueOf(opt)
-  if v.Kind() == reflect.Ptr && v.IsNil() {
-    return s, nil
-  }
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return s, nil
+	}
 
-  u, err := url.Parse(s)
-  if err != nil {
-    return s, err
-  }
+	u, err := url.Parse(s)
+	if err != nil {
+		return s, err
+	}
 
-  qs, err := query.Values(opt)
-  if err != nil {
-    return s, err
-  }
+	qs, err := query.Values(opt)
+	if err != nil {
+		return s, err
+	}
 
-  u.RawQuery = qs.Encode()
-  return u.String(), nil
+	u.RawQuery = qs.Encode()
+	return u.String(), nil
 }
 
 // Returns a new LocalBitcoins API client. If a nil httpClient is provided,
@@ -59,13 +59,13 @@ func addOptions(s string, opt interface{}) (string, error) {
 // perform the authentication for you (such as that provided by the goauth2
 // library).
 func NewClient(httpClient *http.Client) *Client {
-  if httpClient == nil {
-    httpClient = http.DefaultClient
-  }
-  baseURL, _ := url.Parse(defaultBaseURL)
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	baseURL, _ := url.Parse(defaultBaseURL)
 
-  c := &Client{client: httpClient, BaseURL: baseURL, UserAgent: userAgent}
-  return c
+	c := &Client{client: httpClient, BaseURL: baseURL, UserAgent: userAgent}
+	return c
 }
 
 // Creates an API request. A relative URl can be provided in urlStr, in which
@@ -73,89 +73,89 @@ func NewClient(httpClient *http.Client) *Client {
 // should always be specified without a preceding slash. If specified, the
 // value pointed to by body is JSON encoded and included as the request body.
 func (c *Client) NewRequest(method, urlStr string,
-                            body interface{}) (*http.Request, error) {
-  rel, err := url.Parse(urlStr)
-  if err != nil {
-    return nil, err
-  }
+	body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
 
-  u := c.BaseURL.ResolveReference(rel)
+	u := c.BaseURL.ResolveReference(rel)
 
-  buf := new(bytes.Buffer)
-  if body != nil {
-    err := json.NewEncoder(buf).Encode(body)
-    if err != nil {
-      return nil, err
-    }
-  }
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-  req, err := http.NewRequest(method, u.String(), buf)
-  if err != nil {
-    return nil, err
-  }
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
 
-  req.Header.Add("User-Agent", c.UserAgent)
-  return req, nil
+	req.Header.Add("User-Agent", c.UserAgent)
+	return req, nil
 }
 
 // Response is a LocalBitcoins API response. This wraps the standard
 // http.Response returned from LocalBitcoins and provides convenient access to
 // things like pagination links.
 type Response struct {
-  *http.Response
+	*http.Response
 }
 
 // Creates a new Response for the provided http.Response.
 func newResponse(r *http.Response) *Response {
-  response := &Response{Response: r}
-  return response
+	response := &Response{Response: r}
+	return response
 }
 
 // Sends an API request and returns the API response. The API response is
 // decoded and stored in the value pointed to by v, or returned as an error if
 // an API error has occurred.
 func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
-  resp, err := c.client.Do(req)
-  if err != nil {
-    return nil, err
-  }
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  response := newResponse(resp)
+	response := newResponse(resp)
 
-  err = CheckResponse(resp)
-  if err != nil {
-    // even if there was an error, we still return the response body if the
-    // user wants to inspect it further
-    return response, err
-  }
+	err = CheckResponse(resp)
+	if err != nil {
+		// even if there was an error, we still return the response body if the
+		// user wants to inspect it further
+		return response, err
+	}
 
-  if v != nil {
-    err = json.NewDecoder(resp.Body).Decode(v)
-  }
-  return response, err
+	if v != nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
+	return response, err
 }
 
 // An ErrorResponse reports an error caused by an API request.
 type ErrorResponse struct {
-  Response *http.Response
-  Err Error `json:"error"`
+	Response *http.Response
+	Err      Error `json:"error"`
 }
 
 func (r *ErrorResponse) Error() string {
-  return fmt.Sprintf("%v %v: %d - %v %d",
-    r.Response.Request.Method, r.Response.Request.URL,
-    r.Response.StatusCode, r.Err.Message, r.Err.Code)
+	return fmt.Sprintf("%v %v: %d - %v %d",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Err.Message, r.Err.Code)
 }
 
 type Error struct {
-  Message string `json:"message"`
-  Code int `json:"error_code"`
+	Message string `json:"message"`
+	Code    int    `json:"error_code"`
 }
 
 func (e *Error) Error() string {
-  return fmt.Sprintf(`type %d error with message "%v"`, e.Code, e.Message)
+	return fmt.Sprintf(`type %d error with message "%v"`, e.Code, e.Message)
 }
 
 // CheckResponse checks the API response for errors, and returns them if
@@ -164,21 +164,21 @@ func (e *Error) Error() string {
 // body, or a JSON response body that maps to ErrorResponse. Any other
 // response body will be silently ignored.
 func CheckResponse(r *http.Response) error {
-  if c := r.StatusCode; 200 <= c && c <= 299 {
-    return nil
-  }
-  errorResponse := &ErrorResponse{Response: r}
-  data, err := ioutil.ReadAll(r.Body)
-  if err == nil && data != nil {
-    json.Unmarshal(data, errorResponse)
-  }
-  return errorResponse
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && data != nil {
+		json.Unmarshal(data, errorResponse)
+	}
+	return errorResponse
 }
 
 // String is a helper routine that allocates a new string value to store v and
 // returns a pointer to it.
 func String(v string) *string {
-  p := new(string)
-  *p = v
-  return p
+	p := new(string)
+	*p = v
+	return p
 }
--- localbitcoins_test.go
+++ localbitcoins_test.go
@@ -1,208 +1,208 @@
 package localbitcoins
 
 import (
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "net/http/httptest"
-  "net/url"
-  "reflect"
-  "strings"
-  "testing"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"strings"
+	"testing"
 )
 
 var (
-  // HTTP request multiplayer used with test server.
-  mux *http.ServeMux
+	// HTTP request multiplayer used with test server.
+	mux *http.ServeMux
 
-  // localbitcoins client being used
-  client *Client
+	// localbitcoins client being used
+	client *Client
 
-  // test HTTP that provides mock API responses
-  server *httptest.Server
+	// test HTTP that provides mock API responses
+	server *httptest.Server
 )
 
 // Sets up a test HTTP server along with a localbitcoins.Client that is
 // configured to talk to the test server. Tests should register handlers on mux
 // which provide mock responses for the API method being tested.
 func setup() {
-  // test server
-  mux = http.NewServeMux()
-  server = httptest.NewServer(mux)
+	// test server
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
 
-  // localbitcoins client configured to use test server
-  client = NewClient(nil)
-  client.BaseURL, _ = url.Parse(server.URL)
+	// localbitcoins client configured to use test server
+	client = NewClient(nil)
+	client.BaseURL, _ = url.Parse(server.URL)
 }
 
 // teardown closes the test HTTP server.
 func teardown() {
-  server.Close()
+	server.Close()
 }
 
 func testMethod(t *testing.T, r *http.Request, want string) {
-  if want != r.Method {
-    t.Errorf("Request method = %v, want %v", r.Method, want)
-  }
+	if want != r.Method {
+		t.Errorf("Request method = %v, want %v", r.Method, want)
+	}
 }
 
 func testURLParseError(t *testing.T, err error) {
-  if err == nil {
-    t.Errorf("Expected error to be returned")
-  }
-  if err, ok := err.(*url.Error); !ok || err.Op != "parse" {
-    t.Errorf("Expected URL parse error, got %+v", err)
-  }
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+	if err, ok := err.(*url.Error); !ok || err.Op != "parse" {
+		t.Errorf("Expected URL parse error, got %+v", err)
+	}
 }
 
 func TestNewClient(t *testing.T) {
-  c := NewClient(nil)
+	c := NewClient(nil)
 
-  if c.BaseURL.String() != defaultBaseURL {
-    t.Errorf("NewClient BaseURL = %v, want %v", c.BaseURL.String(),
-             defaultBaseURL)
-  }
-  if c.UserAgent != userAgent {
-    t.Errorf("NewClient UserAgent = %v, want %v", c.UserAgent, userAgent)
-  }
+	if c.BaseURL.String() != defaultBaseURL {
+		t.Errorf("NewClient BaseURL = %v, want %v", c.BaseURL.String(),
+			defaultBaseURL)
+	}
+	if c.UserAgent != userAgent {
+		t.Errorf("NewClient UserAgent = %v, want %v", c.UserAgent, userAgent)
+	}
 }
 
 func TestNewRequest(t *testing.T) {
-  c := NewClient(nil)
+	c := NewClient(nil)
 
-  // TODO: Use a struct inside the program for this test.
-  type TestType struct {
-    Field *string `json:"field,omitempty"`
-  }
+	// TODO: Use a struct inside the program for this test.
+	type TestType struct {
+		Field *string `json:"field,omitempty"`
+	}
 
-  inURL, outURL := "/foo", defaultBaseURL + "foo"
-  inBody, outBody := &TestType{Field: String("l")}, `{"field":"l"}` + "\n"
+	inURL, outURL := "/foo", defaultBaseURL+"foo"
+	inBody, outBody := &TestType{Field: String("l")}, `{"field":"l"}`+"\n"
 
-  req, _ := c.NewRequest("GET", inURL, inBody)
+	req, _ := c.NewRequest("GET", inURL, inBody)
 
-  // test that relative URL was expanded
-  if req.URL.String() != outURL {
-    t.Errorf("NewRequest(%v) URL = %v, want %v", inURL, req.URL, outURL)
-  }
+	// test that relative URL was expanded
+	if req.URL.String() != outURL {
+		t.Errorf("NewRequest(%v) URL = %v, want %v", inURL, req.URL, outURL)
+	}
 
-  // test that body was JSON encoded
-  body, _ := ioutil.ReadAll(req.Body)
-  if string(body) != outBody {
-    t.Errorf("NewRequest(%v) Body = %v, want %v", inBody, string(body),
-             outBody)
-  }
+	// test that body was JSON encoded
+	body, _ := ioutil.ReadAll(req.Body)
+	if string(body) != outBody {
+		t.Errorf("NewRequest(%v) Body = %v, want %v", inBody, string(body),
+			outBody)
+	}
 
-  // test that default user-agent is attached to the request
-  userAgent := req.Header.Get("User-Agent")
-  if c.UserAgent != userAgent {
-    t.Errorf("NewRequest() User-Agent = %v, want %v", userAgent, c.UserAgent)
-  }
+	// test that default user-agent is attached to the request
+	userAgent := req.Header.Get("User-Agent")
+	if c.UserAgent != userAgent {
+		t.Errorf("NewRequest() User-Agent = %v, want %v", userAgent, c.UserAgent)
+	}
 }
 
 func TestNewRequest_invalidJSON(t *testing.T) {
-  c := NewClient(nil)
+	c := NewClient(nil)
 
-  type T struct {
-    A map[int]interface{}
-  }
-  _, err := c.NewRequest("GET", "/", &T{})
+	type T struct {
+		A map[int]interface{}
+	}
+	_, err := c.NewRequest("GET", "/", &T{})
 
-  if err == nil {
-    t.Error("Expected error to be returned.")
-  }
-  if err, ok := err.(*json.UnsupportedTypeError); !ok {
-    t.Errorf("Expected a JSON error: got %#v.", err)
-  }
+	if err == nil {
+		t.Error("Expected error to be returned.")
+	}
+	if err, ok := err.(*json.UnsupportedTypeError); !ok {
+		t.Errorf("Expected a JSON error: got %#v.", err)
+	}
 }
 
 func TestNewRequest_badURL(t *testing.T) {
-  c := NewClient(nil)
-  _, err := c.NewRequest("GET", ":", nil)
-  testURLParseError(t, err)
+	c := NewClient(nil)
+	_, err := c.NewRequest("GET", ":", nil)
+	testURLParseError(t, err)
 }
 
 func TestDo(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  type foo struct {
-    A string
-  }
+	type foo struct {
+		A string
+	}
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    if m := "GET"; m != r.Method {
-      t.Errorf("Request method = %v, want %v", r.Method, m)
-    }
-    fmt.Fprint(w, `{"A":"a"}`)
-  })
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if m := "GET"; m != r.Method {
+			t.Errorf("Request method = %v, want %v", r.Method, m)
+		}
+		fmt.Fprint(w, `{"A":"a"}`)
+	})
 
-  req, _ := client.NewRequest("GET", "/", nil)
-  body := new(foo)
-  client.Do(req, body)
+	req, _ := client.NewRequest("GET", "/", nil)
+	body := new(foo)
+	client.Do(req, body)
 
-  want := &foo{"a"}
-  if !reflect.DeepEqual(body, want) {
-    t.Errorf("Response body = %v, want %v", body, want)
-  }
+	want := &foo{"a"}
+	if !reflect.DeepEqual(body, want) {
+		t.Errorf("Response body = %v, want %v", body, want)
+	}
 }
 
 func TestDo_httpError(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    http.Error(w, "Bad Request", 400)
-  })
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Bad Request", 400)
+	})
 
-  req, _ := client.NewRequest("GET", "/", nil)
-  _, err := client.Do(req, nil)
+	req, _ := client.NewRequest("GET", "/", nil)
+	_, err := client.Do(req, nil)
 
-  if err == nil {
-    t.Error("Expected HTTP 400 error.")
-  }
+	if err == nil {
+		t.Error("Expected HTTP 400 error.")
+	}
 }
 
 // Test handling of an error caused by the internal http client's Do()
 // function. A redirect loop is pretty unlikely to occur within the
 // LocalBitcoins API, but does allow us to exercise the right code path.
 func TestDo_redirectLoop(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "/", http.StatusFound)
-  })
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
 
-  req, _ := client.NewRequest("GET", "/", nil)
-  _, err := client.Do(req, nil)
+	req, _ := client.NewRequest("GET", "/", nil)
+	_, err := client.Do(req, nil)
 
-  if err == nil {
-    t.Error("Expected error to be returned.")
-  }
-  if err, ok := err.(*url.Error); !ok {
-    t.Errorf("Expected a URL error; got %#v.", err)
-  }
+	if err == nil {
+		t.Error("Expected error to be returned.")
+	}
+	if err, ok := err.(*url.Error); !ok {
+		t.Errorf("Expected a URL error; got %#v.", err)
+	}
 }
 
 func TestCheckResponse(t *testing.T) {
-  res := &http.Response{
-    Request: &http.Request{},
-    StatusCode: http.StatusBadRequest,
-    Body: ioutil.NopCloser(strings.NewReader(`{
+	res := &http.Response{
+		Request:    &http.Request{},
+		StatusCode: http.StatusBadRequest,
+		Body: ioutil.NopCloser(strings.NewReader(`{
       "error": {"message": "m", "error_code": 7}}`)),
-  }
-  err := CheckResponse(res).(*ErrorResponse)
+	}
+	err := CheckResponse(res).(*ErrorResponse)
 
-  if err == nil {
-    t.Errorf("Expected error response.")
-  }
+	if err == nil {
+		t.Errorf("Expected error response.")
+	}
 
-  want := &ErrorResponse{
-    Response: res,
-    Err: Error{Message: "m", Code: 7},
-  }
-  if !reflect.DeepEqual(err, want) {
-    t.Errorf("Error = %#v, want %#v", err, want)
-  }
+	want := &ErrorResponse{
+		Response: res,
+		Err:      Error{Message: "m", Code: 7},
+	}
+	if !reflect.DeepEqual(err, want) {
+		t.Errorf("Error = %#v, want %#v", err, want)
+	}
 }
